0226baac365cf6e1fa6e4cb173f0d513214b684f
Your Name <your-email@example.com>
2026-04-12 09:33:43 +0200

    Final cleanup for serializers.py

--- dataAboutUser/serializers.py
+++ dataAboutUser/serializers.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
-from .models import Food, FoodAlternative, HealthProfile
 from django.contrib.auth.models import User
-from .models import HealthProfile
-from .models import ScanHistory
+from .models import Food, FoodAlternative, HealthProfile, ScanHistory
 
+# 1. سيرياليزر المستخدم (للتسجيل والدخول)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+# 2. سيرياليزر الأطعمة
 class FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Food
         fields = '__all__'
 
+# 3. سيرياليزر البدائل الصحية
 class FoodAlternativeSerializer(serializers.ModelSerializer):
-    # بنجيب تفاصيل الأكلة الأصلية والبديلة كاملة مش بس الـ ID
     original_food = FoodSerializer(read_only=True)
     suggested_alternative = FoodSerializer(read_only=True)
 
@@ -19,37 +28,18 @@ class FoodAlternativeSerializer(serializers.ModelSerializer):
         model = FoodAlternative
         fields = ['original_food', 'suggested_alternative', 'reason_why']
 
-
-        
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}} # عشان الباسورد ميرجعش في الـ API
-
-    def create(self, validated_data):
-        # إنشاء المستخدم وتشفير الباسورد أوتوماتيك
-        user = User.objects.create_user(**validated_data)
-        return user
-    
-
-
-
+# 4. سيرياليزر تاريخ المسح (الاسكان)
 class ScanHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ScanHistory
         fields = ['id', 'user', 'detected_food', 'ai_analysis', 'image', 'created_at']
 
-
-        class HealthProfileSerializer(serializers.ModelSerializer):
-            class Meta:
-                model = HealthProfile
-                fields = ['height', 'weight', 'age', 'gender', 'activity_level', 'daily_calories_goal']
-
-from .models import HealthProfile
-
+# 5. سيرياليزر الملف الصحي (اللي ضفنا فيه الحقول الجديدة)
 class HealthProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = HealthProfile
-        fields = '__all__' # أو حددي الحقول اللي محتاجاها
+        fields = [
+            'id', 'user', 'height', 'weight', 'age', 'gender', 
+            'daily_calories_goal', 'has_diabetes', 
+            'has_blood_pressure', 'food_allergies', 'chronic_diseases'
+        ]
